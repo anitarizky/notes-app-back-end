9b4f77443fd2524385bab81231abc6f79a585c1a
Anita Rizky <anitarizky_agustina@yahoo.co.id>
2022-05-12 11:22:49 +0700

    resource catatan

--- src/handler.js
+++ src/handler.js
@@ -3,7 +3,7 @@ const { nanoid } = require(`nanoid`);
 const notes = require(`./notes`);
 
 const addNoteHandler = (request, h) => {
-  const { title, tags, body } = request.payload;
+  const { title = `untitled`, tags, body } = request.payload;
 
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
@@ -35,7 +35,7 @@ const addNoteHandler = (request, h) => {
     status: `fail`,
     message: `Catatan gagal ditambahkan`,
   });
-  response.code(500);
+  response.code(400);
   return response;
 };
 
@@ -62,7 +62,7 @@ const getNoteByIdHandler = (request, h) => {
     status: `fail`,
     message: `Catatan tidak ditemukan`,
   });
-  response.code(400);
+  response.code(404);
   return response;
 };
 
@@ -96,7 +96,9 @@ const editNoteByIdHandler = (request, h) => {
 
 const deleteNoteByIdHandler = (request, h) => {
   const { id } = request.params;
+
   const index = notes.findIndex((note) => note.id === id);
+
   if (index !== -1) {
     notes.splice(index, 1);
     const response = h.response({
@@ -106,6 +108,7 @@ const deleteNoteByIdHandler = (request, h) => {
     response.code(200);
     return response;
   }
+
   const response = h.response({
     status: `fail`,
     message: `Catatan gagal dihapus. Id tidak ditemukan`,
---
+++ src/services/inMemory/NotesService.js
@@ -0,0 +1,84 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable require-jsdoc */
+const { nanoid } = require(`nanoid`);
+/* eslint "require-jsdoc": ["error", {
+    "require": {
+        "FunctionDeclaration": true,
+        "MethodDefinition": true,
+        "ClassDeclaration": true,
+        "ArrowFunctionExpression": true,
+        "FunctionExpression": true
+    }
+}]*/
+
+/**
+ * A class that can return
+ */
+class NotesService {
+  constructor() {
+    this._notes = [];
+  }
+
+  addNote({ title, body, tags }) {
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+      title,
+      tags,
+      body,
+      id,
+      createdAt,
+      updatedAt,
+    };
+
+    this._notes.push(newNote);
+    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
+    if (!isSuccess) {
+      throw new Error(`Catatan gagal ditambahkan`);
+    }
+
+    return id;
+  }
+
+  getNotes() {
+    return this._notes;
+  }
+
+  getNoteById() {
+    const note = note.filter((n) => n.id === id)[0];
+    if (!note) {
+      throw new Error(`Catatan tidak ditemukan`);
+    }
+    return note;
+  }
+
+  editNoteById(Id, { title, body, tags }) {
+    const index = this._notes.findIndex((note) => note.id === Id);
+
+    if (index === -1) {
+      throw new Error(`Gagal memperbarui catatan. Id tidak ditemukan`);
+    }
+
+    const updatedAt = new Date().toISOString();
+
+    this._notes[index] = {
+      ...this._notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+  }
+
+  deleteNoteById(id) {
+    const index = this._notes.findIndex((note) => note.id === id);
+    if (index === -1) {
+      throw new Error(`Catatan gagal dihapus. Id tidak ditemukan`);
+    }
+    this._notes.splice(index, 1);
+  }
+}
+
+module.exports = NotesService;
